3cc8e7f0a6360911adb680a9439f5e029887b399
Radu Zubascu <rzubascu@smart.care>
2016-08-23 11:19:56 +0300

    - Updated FontAwesomeView

--- FontAwesome/FontAwesomeView.swift
+++ FontAwesome/FontAwesomeView.swift
@@ -32,43 +32,41 @@ import UIKit
         }
     }
     
-    private var iconView:UILabel!
+    private var iconView = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupViews()
+        self.setupViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupViews()
+        self.setupViews()
     }
     
     override func prepareForInterfaceBuilder() {
-        setupViews()
-        
+        self.setupViews()
     }
     
     
     /// Add a UILabel subview containing FontAwesome icon
     func setupViews(){
-        self.iconView = UILabel()
         // Fits icon in the view
-        iconView.font = UIFont.fontAwesomeOfSize(bounds.size.width < bounds.size.height ? bounds.size.width : bounds.size.height)
-        iconView.textAlignment = NSTextAlignment.Center
-        iconView.text = String.fontAwesomeIconWithCode(self.iconCode)
-        iconView.textColor = self.tintColor
+        self.iconView.textAlignment = NSTextAlignment.center
+        self.iconView.text = String.fontAwesomeIconWithCode(self.iconCode)
+        self.iconView.textColor = self.tintColor
         self.addSubview(iconView)
     }
     
     override func tintColorDidChange() {
-        iconView.textColor = self.tintColor
+        self.iconView.textColor = self.tintColor
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.clipsToBounds = true
-        iconView.frame = CGRectMake(0, 0, bounds.size.width, bounds.size.height)
+        self.iconView.font = UIFont.fontAwesomeOfSize(bounds.size.width < bounds.size.height ? bounds.size.width : bounds.size.height)
+        self.iconView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: bounds.size.width, height: bounds.size.height))
     }
     
 }
